e3ff8fbbbd1cee96c6ef571bc3c94402b78caef4
Kevin Rethwisch <kevinrethwisch@gmail.com>
2024-12-27 18:17:01 +0800

    yaml

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,12 @@
 import { defineConfig } from 'astro/config';
 import vue from '@astrojs/vue';
+import yaml from '@rollup/plugin-yaml';
+
 export default defineConfig({
   srcDir: 'src/astro',
   compressHTML: false,
-  integrations: [vue()]
+  integrations: [vue()],
+  vite: {
+    plugins: [yaml()]
+  }
 });
