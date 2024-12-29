0d97b247d6ca024f0c9c3f735d6d4edbb7b91773
Kevin Rethwisch <kevinrethwisch@gmail.com>
2024-12-29 18:51:39 +0800

    Breadcrumbs.astro

---
+++ components/Breadcrumb.vue
@@ -0,0 +1,18 @@
+<template lang="pug">
+nav.breadcrumbs#breadcrumb(aria-label="breadcrumb" v-html="breadcrumbs")
+//- nav(v-html="breadcrumbs")
+</template>
+
+<script setup lang="ts">
+import { defineProps, onMounted } from 'vue';
+import { formatBreadcrumbs } from '@/utils/breadcrumbUtils';
+
+// Define props to accept a single HTML string
+const props = defineProps({
+  breadcrumbs: String,
+});
+
+onMounted(() => {
+  formatBreadcrumbs('#breadcrumb')
+})
+</script>
