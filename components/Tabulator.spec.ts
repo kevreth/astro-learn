42999400630366ef2b79460a1d9f4614bd901b8b
Kevin Rethwisch <kevinrethwisch@gmail.com>
2025-01-03 10:38:36 +0800

    test

--- components/Tabulator.spec.ts
+++ components/Tabulator.spec.ts
@@ -4,17 +4,6 @@ import { expect, test, vi, beforeEach } from 'vitest';
 import { JSDOM } from 'jsdom';
 import Tabulator from './Tabulator.astro';
 import axios from 'axios';
-// vi.mock('../src/scripts/tabulator/tabulator', () => ({
-//     mounted: vi.fn(), 
-//     columns: []
-// }));
-beforeEach(async () => {
-    console.log("starting server")
-    await startStaticServer(3000, './dist')
-    const response = await axios.get('http://localhost:3000/tabulator.js');
-    expect(response.status).toBe(200);
-    expect(response.data).toContain('"Tabulator loaded"');
-})
 test('Tabulator with prop', async () => {
     const container = await AstroContainer.create();
     const dom = new JSDOM('', { url: "http://localhost" });
@@ -31,6 +20,17 @@ test('Tabulator with prop', async () => {
     const props = doc.getElementById('props') as HTMLElement;
     const priority = parseInt(props.dataset.priority as string);
     expect(priority).toEqual(1);
+});
+test('Tabulator with prop', async () => {
+    console.log("starting server")
+    await startStaticServer(3000, './dist')
+    const response = await axios.get('http://localhost:3000/tabulator.js');
+    expect(response.status).toBe(200);
+    expect(response.data).toContain('"Tabulator loaded"');
+
+    const container = await AstroContainer.create();
+    const dom = new JSDOM('', { url: "http://localhost" });
+    const result = await container.renderToString(Tabulator, {props: { tabulatorText: "World", priority: 1, doc: dom.window.document }});
     const dom3 = new JSDOM(result, {runScripts: "dangerously", resources: "usable"})
     const doc2 = dom3.window.document
     console.log(doc2)
