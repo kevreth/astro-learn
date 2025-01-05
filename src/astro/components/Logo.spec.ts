46ecac3651bd3aea76db08acd6df6317dbfc21b3
Kevin Rethwisch <kevinrethwisch@gmail.com>
2025-01-05 14:27:00 +0800

    logo

--- src/astro/components/Logo.spec.ts
+++ src/astro/components/Logo.spec.ts
@@ -3,7 +3,7 @@ import { expect, test } from 'vitest';
 import Logo from './Logo.astro';
 test('Logo with prop', async () => {
   const container = await AstroContainer.create();
-  const result = await container.renderToString(Logo, {props: { src: "Hello", alt:"World" }});
+  const result = await container.renderToString(Logo, {props: {logo: { src: "Hello", alt:"World" }}});
   expect(result).not.toContain("24phlaweifh");
   expect(result).toContain("Hello");
   expect(result).toContain("World");
