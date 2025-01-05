9cbcc3380b311e134b9aa1b279d67207a1ca645e
Kevin Rethwisch <kevinrethwisch@gmail.com>
2025-01-05 14:20:03 +0800

    restructuring

--- src/astro/composables/usePageSetup.ts
+++ src/astro/composables/usePageSetup.ts
@@ -1,9 +1,8 @@
 import data from './data.json';
 import setupPage from '../../scripts/stdHead'
-import { setHead } from '@astrojs/head-utils'
-export function usePageSetup(data: any) {
+export function usePageSetup(data: any): void {
   const baseURL = import.meta.env.PUBLIC_BASE_URL
-  const canonicalURL = `${baseURL}${Astro.url.pathname}`
+  const canonicalURL = `${baseURL}${data.pathname}`
 
   setHead({
     link: [
