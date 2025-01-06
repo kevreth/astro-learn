e9e0371bb0760a9212cca08c1766b0862858db61
Kevin Rethwisch <kevinrethwisch@gmail.com>
2025-01-06 19:46:14 +0800

    fixed.header.and.footer

--- src/astro/components/Footer.spec.ts
+++ src/astro/components/Footer.spec.ts
@@ -3,7 +3,7 @@ import { expect, test } from 'vitest';
 import Footer from './Footer.astro';
 test('Footer with prop', async () => {
   const container = await AstroContainer.create();
-  const result = await container.renderToString(Footer, {props: { footerText: "Hello" }});
+  const result = await container.renderToString(Footer, {props: { footer: "Hello" }});
   expect(result).not.toContain("24phlaweifh");
   expect(result).toContain("Hello");
 })
--- src/astro/components/Header.spec.ts
+++ src/astro/components/Header.spec.ts
@@ -3,8 +3,7 @@ import { expect, test } from 'vitest';
 import Header from './Header.astro';
 test('Header with prop', async () => {
   const container = await AstroContainer.create();
-  const result = await container.renderToString(Header, {props: { title: "Hello", subtitle:"World" }});
+  const result = await container.renderToString(Header, {props: { header: "Hello" }});
   expect(result).not.toContain("24phlaweifh");
   expect(result).toContain("Hello");
-  expect(result).toContain("World");
 })
