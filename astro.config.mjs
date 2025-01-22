bff7e1db51e30e8a5d562faa141a312390d43334
Kevin Rethwisch <kevinrethwisch@gmail.com>
2025-01-22 10:06:34 +0800

    resolved

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from 'astro/config';
 import vue from '@astrojs/vue';
 import yaml from '@rollup/plugin-yaml';
-
+import sitemap from '@astrojs/sitemap'
 export default defineConfig({
   srcDir: 'src/astro',
   compressHTML: false,
-  integrations: [vue()],
+  site: 'https://inquirita.com',
+  integrations: [vue(),sitemap()],
   vite: {
     plugins: [yaml()]
   }
