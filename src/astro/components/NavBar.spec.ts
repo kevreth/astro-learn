70797feb6db63ee80828ee63d2f0e6509aeefda7
Kevin Rethwisch <kevinrethwisch@gmail.com>
2025-02-19 18:32:35 +0800

    done

--- src/astro/components/NavBar.spec.ts
+++ src/astro/components/NavBar.spec.ts
@@ -4,7 +4,7 @@ import NavBar from './NavBar.astro';
 
 test('NavBar with props', async () => {
   const container = await AstroContainer.create();
-  const nav = [[{ path: '/home', label: 'Home' }],[{ path: '/about', label: 'About' }]];
+  const nav = [{ path: '/home', label: 'Home' },{ path: '/about', label: 'About' }];
   const result = await container.renderToString(NavBar, { props: { nav } });
   expect(result).toContain('Home');
   expect(result).toContain('About');
